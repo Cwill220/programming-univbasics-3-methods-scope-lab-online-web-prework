5a9ccda3bbb964ac5ce8f61e3662a5782e9134ab
Caleb Willis <calebwillis50@yahoo.com>
2019-12-07 20:45:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,4 +3,15 @@ def mario
   puts phrase
 end
 
+def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts status
+end
+
+def link
+  phrase = "It's Dangerous To Go ALone! Take This."
+  puts phrase
+end
+
+def any_phrase
   
